5a1ac7e03c24d854da357b5c3b33f25e9409730b
mudassir733 <deadlool45@gmail.com>
2025-08-22 16:49:09 +0500

    updated frontend middleware for google auth

--- src/middleware.js
+++ src/middleware.js
@@ -2,16 +2,20 @@ import { NextResponse } from 'next/server'
 
 export function middleware(request) {
     const token = request.cookies.get('access_token')?.value
+    const googleToken = request.cookies.get('connect.sid')?.value
     const { pathname } = request.nextUrl
 
-    if (!token && pathname === '/') {
+
+    if (!token && !googleToken && pathname === '/') {
         const url = request.nextUrl.clone()
         url.pathname = '/sign-up'
         return NextResponse.redirect(url)
     }
 
 
-    if (token && (pathname === '/login' || pathname === '/sign-up')) {
+
+
+    if (token && googleToken && (pathname === '/login' || pathname === '/sign-up')) {
         const url = request.nextUrl.clone()
         url.pathname = '/'
         return NextResponse.redirect(url)
